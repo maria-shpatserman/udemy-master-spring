79037b5d76ee93e3806688128d09bc14dcc57deb
maria-shpatserman <sokunova@gmail.com>
2024-10-26 22:46:20 +0800

    Master Spring: chapter 2

---
+++ master-one/src/main/java/ru/netunix/masterspring/master_one/MasterOneApplication.java
@@ -0,0 +1,13 @@
+package ru.netunix.masterspring.master_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterOneApplication.class, args);
+	}
+
+}
---
+++ master-one/src/test/java/ru/netunix/masterspring/master_one/MasterOneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netunix.masterspring.master_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
